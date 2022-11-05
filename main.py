33622d3e5a5fe800cb57c01def356824f3e1aeb8
Mikateko <52527214+Mikateko18@users.noreply.github.com>
2022-11-05 10:59:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,9 @@ from flask import  Flask
 from app import views
 import matplotlib.pyplot as plt
 
-app = Flask(__name__)
-app.add_url_rule(rule='/',endpoint='home',view_func=views.faceApp)
-app.add_url_rule(rule='/faceApp/',
-                 endpoint='faceApp',
-                 view_func=views.faceApp,
-                 methods=['GET','POST'])
+app = Flask(__name__, template_folder='templates')
 
+
+app.add_url_rule('/', 'faceApp',views.faceApp, methods=['GET','POST'])
 if __name__ == "__main__":
     app.run(debug=True)
-
